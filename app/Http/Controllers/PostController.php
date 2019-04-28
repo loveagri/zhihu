4938fcaf733922f42aeb1b051c14c39324b1b3d5
loveagri <282656050@qq.com>
2019-04-28 20:04:37 +0800

    img

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,6 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::orderBy('created_at','desc')->paginate(6);
-
         return view('post.index',compact('posts'));
     }
 
