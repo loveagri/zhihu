e8f49d79663ffc2fd8014982e576dd17c0d43e19
loveagri <984070691@qq.com>
2019-05-07 18:40:45 +0800

    web

--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
     <div class="col-sm-8 blog-main mt-3">
-        <form action="/user/me/setting" method="POST">
+        <form  action="/user/me/setting" method="POST" enctype="multipart/form-data">
+            @csrf
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">用户名</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" placeholder="">
                 </div>
             </div>
-
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">头像</label>
                 <div class="col-sm-4 row align-items-center">
                     <input type="file" class="form-control-file" value="用户名" name="avatar">
-                    <img src="https://randomuser.me/api/portraits/women/23.jpg" alt="" class="img-rounded mt-4" style="border-radius:500px;">
+                    <img class="preview_img"  src="{{$me->avatar}}" alt="" class="img-rounded mt-4" style="border-radius:500px;">
                 </div>
             </div>
             <button type="submit" class="btn btn-primary">修改</button>
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ Route::post('/login', '\App\Http\Controllers\LoginController@login');
 
 Route::group(['middleware' => 'auth:web'], function () {
     Route::get('/logout', '\App\Http\Controllers\LoginController@logout');
-    Route::get('/user/me/setting', '\App\Http\Controllers\UserController@setting');
-    Route::post('/user/me/setting', '\App\Http\Controllers\UserController@settingStore');
 
     Route::get('/posts/search', '\App\Http\Controllers\PostController@search');
 
@@ -39,6 +37,9 @@ Route::group(['middleware' => 'auth:web'], function () {
     Route::get('/user/{user}', '\App\Http\Controllers\UserController@show');
     Route::post('/user/{user}/fan', '\App\Http\Controllers\UserController@fan');
     Route::post('/user/{user}/unfan', '\App\Http\Controllers\UserController@unfan');
+
+    Route::get('/user/me/setting', '\App\Http\Controllers\UserController@setting');
+    Route::post('/user/me/setting', '\App\Http\Controllers\UserController@settingStore');
 
     Route::get('/topic/{topic}', '\App\Http\Controllers\TopicController@show');
     Route::post('/topic/{topic}/submit', '\App\Http\Controllers\TopicController@submit');
