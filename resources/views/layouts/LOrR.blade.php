fee3d380529057134850ae992a866ec6a675a742
loveagri <984070691@qq.com>
2019-05-27 15:44:15 +0800

    222

--- resources/views/layouts/LOrR.blade.php
+++ resources/views/layouts/LOrR.blade.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>地农空间</title>
+    <title>静思田园</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,12 +2,12 @@
 
     <div class="card mb-3" >
       <div class="card-header">
-        欢迎！欢迎来到地农空间
+        欢迎！欢迎来到静思田园
     </div>
 
    <ul class="list-group list-group-flush">
     <li class="list-group-item">    <p>
-        <strong><a href="/">地农空间</a></strong> 基于 Laravel5.7 构建
+        <strong><a href="/">静思田园</a></strong> 基于 Laravel5.7 构建
     </p></li>
 </ul>
 </div>
