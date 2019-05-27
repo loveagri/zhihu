985deecf8af453b87983b3cc0d8a9a052d57c581
loveagri <984070691@qq.com>
2019-05-27 12:15:58 +0800

    title

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,12 +2,12 @@
 
     <div class="card mb-3" >
       <div class="card-header">
-        欢迎！欢迎来到简书网站
+        欢迎！欢迎来到地农空间
     </div>
 
    <ul class="list-group list-group-flush">
     <li class="list-group-item">    <p>
-        <strong><a href="/">简书网站</a></strong> 基于 Laravel5.4 构建
+        <strong><a href="/">地农空间</a></strong> 基于 Laravel5.7 构建
     </p></li>
 </ul>
 </div>
