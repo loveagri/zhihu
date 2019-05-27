343bb0b09c8fdccc1f815e4f28b05aa3c2c7b754
loveagri <984070691@qq.com>
2019-05-27 12:04:53 +0800

    url

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>知乎</title>
+    <title>地农空间</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
