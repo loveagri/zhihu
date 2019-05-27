b37ef26d3dccb2a84a9748b29f370cb506c0405f
loveagri <984070691@qq.com>
2019-05-27 12:14:30 +0800

    beian

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="blog-footer">
-    <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>冀ICP备18030681号</p>
     <p>
         <a href="#">Back to top</a>
     </p>
