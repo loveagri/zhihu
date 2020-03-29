bfa95a89169e15cbedee76cb7748ce12b59d7ea1
loveagri <282656050@qq.com>
2020-03-29 22:27:25 +0700

    finish

--- resources/views/layouts/LOrR.blade.php
+++ resources/views/layouts/LOrR.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" type="image/x-icon" href="image/au.ico" />
     <title>AU Forum</title>
 
     <!-- Bootstrap core CSS -->
