4eda39b6da76a821f1c915e08d826205fbedda96
loveagri <282656050@qq.com>
2020-04-04 16:27:10 +0700

    finish

--- config/logging.php
+++ config/logging.php
@@ -36,7 +36,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['month'],
             'ignore_exceptions' => true,
         ],
 
